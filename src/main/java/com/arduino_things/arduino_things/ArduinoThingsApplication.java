3709c2a9757d12da5cb1b40bec14c132310a32cf
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-09-23 10:43:40 +0200

    first commit

---
+++ src/main/java/com/arduino_things/arduino_things/ArduinoThingsApplication.java
@@ -0,0 +1,13 @@
+package com.arduino_things.arduino_things;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arduino_things/arduino_things/ArduinoThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arduino_things.arduino_things;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArduinoThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
